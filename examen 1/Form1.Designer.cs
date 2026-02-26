4ddfd3a224faf25d3413dddc51021499242c074a
Edgarjr03 <Edgarjr5354@gmail.com>
2026-02-26 15:45:38 -0600

    Agregar archivos de proyecto.

---
+++ examen 1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace examen_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txb_SueldoMensual = new TextBox();
+            label1 = new Label();
+            btn_Calcular = new Button();
+            label2 = new Label();
+            txb_ISR = new TextBox();
+            label6 = new Label();
+            txb_SueldoNeto = new TextBox();
+            SuspendLayout();
+            // 
+            // txb_SueldoMensual
+            // 
+            txb_SueldoMensual.Location = new Point(280, 61);
+            txb_SueldoMensual.Name = "txb_SueldoMensual";
+            txb_SueldoMensual.Size = new Size(150, 31);
+            txb_SueldoMensual.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(280, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(138, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Sueldo Mensual";
+            // 
+            // btn_Calcular
+            // 
+            btn_Calcular.Location = new Point(298, 123);
+            btn_Calcular.Name = "btn_Calcular";
+            btn_Calcular.Size = new Size(112, 34);
+            btn_Calcular.TabIndex = 2;
+            btn_Calcular.Text = "Calcular";
+            btn_Calcular.UseVisualStyleBackColor = true;
+            btn_Calcular.Click += btn_Calcular_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(120, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 25);
+            label2.TabIndex = 10;
+            label2.Text = "ISR";
+            // 
+            // txb_ISR
+            // 
+            txb_ISR.Location = new Point(26, 123);
+            txb_ISR.Name = "txb_ISR";
+            txb_ISR.Size = new Size(234, 31);
+            txb_ISR.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(501, 95);
+            label6.Name = "label6";
+            label6.Size = new Size(108, 25);
+            label6.TabIndex = 14;
+            label6.Text = "Sueldo neto";
+            // 
+            // txb_SueldoNeto
+            // 
+            txb_SueldoNeto.Location = new Point(436, 123);
+            txb_SueldoNeto.Name = "txb_SueldoNeto";
+            txb_SueldoNeto.Size = new Size(226, 31);
+            txb_SueldoNeto.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(710, 189);
+            Controls.Add(label6);
+            Controls.Add(txb_SueldoNeto);
+            Controls.Add(label2);
+            Controls.Add(txb_ISR);
+            Controls.Add(btn_Calcular);
+            Controls.Add(label1);
+            Controls.Add(txb_SueldoMensual);
+            Name = "Form1";
+            Text = "Calculador de ISR y Sueldo neto";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txb_SueldoMensual;
+        private Label label1;
+        private Button btn_Calcular;
+        private Label label2;
+        private TextBox txb_ISR;
+        private Label label6;
+        private TextBox txb_SueldoNeto;
+    }
+}
---
+++ examen 1/Form1.cs
@@ -0,0 +1,71 @@
+namespace examen_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal sueldo = Convert.ToDecimal(txb_SueldoMensual.Text);
+                if (sueldo <= 0)
+                {
+                    MessageBox.Show("Ingrese un sueldo mayor que 0.");
+                    return;
+                }
+                decimal isr = CalcularISR(sueldo);
+                decimal sueldoNeto = sueldo - isr;
+                txb_ISR.Text = $"ISR: {isr:C}";
+                txb_SueldoNeto.Text = $"Sueldo Neto: {sueldoNeto:C}";
+            }
+            catch
+            { MessageBox.Show("Ingrese un número válido.").; }
+        }
+
+        private decimal CalcularISR(decimal sueldo)
+        {
+            var rangos = new[] {
+            new {Inf=0.01m, Sup=746.04m, Cuota=0.00m, Porc=0.0192m},
+            new {Inf=746.05m, Sup=6332.05m, Cuota=14.32m, Porc=0.0640m},
+            new {Inf=6332.06m, Sup=11128.01m, Cuota=371.83m, Porc=0.1088m},
+            new {Inf=11128.02m, Sup=12935.82m, Cuota=893.63m, Porc=0.1600m},
+            new {Inf=12935.82m, Sup=15487.71m, Cuota=13186.26m, Porc=0.1792m},
+            new {Inf=15487.72m, Sup=31236.49m, Cuota=1972.12m, Porc=0.2136m},
+            new {Inf= 31236.50m, Sup=49233.00m, Cuota=5850.76m, Porc=0.2352m},
+            new {Inf= 49233.01m, Sup=93993.90m, Cuota=10837.02m, Porc=0.30m},
+            new {Inf= 93993.91m, Sup=125325.20m, Cuota=26177.04m, Porc=0.32m},
+            new {Inf=125325.21m, Sup=375975.61m, Cuota=38243.68m, Porc=0.34m},
+            new {Inf=375975.62m, Sup=999999m, Cuota=142119.38m, Porc=0.35m} };
+
+            foreach (var r in rangos)
+            {
+                if (sueldo >= r.Inf && sueldo <= r.Sup)
+                {
+                    return r.Cuota + (sueldo - r.Inf) * r.Porc;
+                }
+            }
+            return 0;
+        }
+    
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
